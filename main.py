4dc1be0711bea72eb0a04c182df18882cce75e47
sonOfArent <humbleogier@gmail.com>
2023-03-21 09:11:54 -0500

    done

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from PIL import Image
+import os
 
-img = Image.open("./images/bookshelf.png")
+def increaseScale(folder_dir, filename, resizeNumber): # Folder_dir includes slashes.
+  
+  img = Image.open(f"{folder_dir}{filename}")
+  newSize = (img.size[0] * resizeNumber, img.size[1] * resizeNumber)
 
-print(img.mode)
-print(img.size)
+  img = img.resize(newSize, resample=Image.NEAREST)
+  img.save(f"{folder_dir}{filename}")
+
+folder_dir = "./images/"
+resizeNumber = 6
+
+for image in os.listdir(folder_dir): increaseScale(folder_dir, image, resizeNumber)
